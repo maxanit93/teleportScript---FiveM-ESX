220b45bc9e71b957705c604fdc83c4c8a943234c
momo.agent <maxanit93@gmail.com>
2025-09-17 16:39:38 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,7 @@
+Config = {}
+
+Config.Teleports = {
+    airport = {x = 200.0, y = -900.0, z = 30.0},
+    ambulance = {x = 925.0, y = 45.0, z = 80.0}
+    police = {x = -350.0, y = -120.0, z = 39.0}
+}
